f51f6f23f2ec3ef5658a0700405323f5b3a0bfa0
Spik <oleg.spinzhar@gmail.com>
2019-11-25 00:40:07 +0200

    edit getLastRowInRange func

--- Spreadsheets/getLastRowInRange/getLastRowInRange.js
+++ Spreadsheets/getLastRowInRange/getLastRowInRange.js
@@ -1,6 +1,7 @@
 /**
  * Returns the position of the last row that has content in the range.
  * @param {Object} range https://developers.google.com/apps-script/reference/spreadsheet/range
+ * @return {number} lastRow
  **/
 function getLastRowInRange(range) {
   var lastRow = 0;
